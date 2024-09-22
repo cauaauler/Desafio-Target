387f298018b6194f1325a2111655745964f8c61b
Cauã Auler <cauaauler@gmail.com>
2024-09-22 13:04:45 -0300

    first commit

---
+++ 02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int INDICE = 12, SOMA = 0, K = 1; 
+        try {
+            while (K < INDICE) {
+                K = K + 1;
+                SOMA = SOMA + K;
+            }
+        } catch (Exception e) {
+            Console.WriteLine("Erro: " + e.Message);
+            Console.WriteLine("StackTrace: " + e.StackTrace);
+        }
+        Console.WriteLine(SOMA);
+    }
+}
